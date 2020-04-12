3488e34d4df57a335ae111ae1d0bc0e174c34d8d
guilhermefaleiros <guilhermefaleiros2000@gmail.com>
2020-04-12 11:56:59 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  port: '5432',
+  username: 'postgres',
+  password: '12345',
+  database: 'radiodb',
+};
---
+++ src/database/migrations/20200412145129-create-users.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: false,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  }),
+
+  down: (queryInterface) => queryInterface.dropTable('users'),
+};
